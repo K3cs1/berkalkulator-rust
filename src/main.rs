4841c7ce461e691d451850a2269abe74499255e3
K3cs1 <kecskemetip@gmail.com>
2024-02-27 20:02:20 +0100

    Full english translation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use gettext::Catalog;
 use slint::{SharedString, Weak};
 use std::collections::HashMap;
+use std::fs::File;
 
 slint::include_modules!();
 
@@ -18,12 +20,14 @@ enum Jarulek {
 struct Berkalkulator {}
 
 impl Berkalkulator {
-    fn calculate_netto_ber(brutto_ber: f64) -> Result<String, String> {
+    fn calculate_netto_ber(brutto_ber: f64, catalog: Catalog) -> Result<String, String> {
         if brutto_ber <= ZERO {
-            return Err("A megadott érték kisebb mint egy!".to_owned());
+            let error_msg = catalog.gettext("A megadott érték kisebb mint egy!");
+            return Err(error_msg.to_owned());
         }
         if brutto_ber > ONE_HUNDRED_MILLION {
-            return Err("A megadott érték túl magas!".to_owned());
+            let error_msg = catalog.gettext("A megadott érték túl magas!");
+            return Err(error_msg.to_owned());
         }
         let jarulekok: Vec<Jarulek> = Self::init_jarulekok();
         let mut sum_of_jarulekok: f64 = ZERO;
@@ -53,7 +57,16 @@ impl Berkalkulator {
             }
         }
         let netto_num: f64 = brutto_ber * (ONE - sum_of_jarulekok);
-        let result: String = format!("Járulékok: \n\nNyugdíj-biztosítási járulék: {:.2} Ft\nPénzbeni Egészségbiztosítási járulék: {:.2} Ft\nTermészetbeni Egészségbiztosítási járulék: {:.2} Ft\nSZJA (személyi jövedelemadó): {:.2} Ft\nMunkaerő-piaci járulék: {:.2} Ft\n\nNettó havi bér: {:.2} Ft", calculated_jarulekok_map.get("nyugdij_bizt").unwrap(), calculated_jarulekok_map.get("penzbeni_egeszseg_bizt").unwrap(), calculated_jarulekok_map.get("term_egeszseg_bizt").unwrap(), calculated_jarulekok_map.get("szja").unwrap(), calculated_jarulekok_map.get("munkaero_piaci").unwrap(), netto_num);
+        let jarulekok_text = catalog.gettext("Járulékok");
+        let nyugdij_bizt_jarulek_text = catalog.gettext("Nyugdíj-biztosítási járulék");
+        let penzbeni_egeszsegbizt_jarulek_text =
+            catalog.gettext("Pénzbeni Egészségbiztosítási járulék");
+        let termeszetbeni_egeszsegbizt_jarulek_text =
+            catalog.gettext("Természetbeni Egészségbiztosítási járulék");
+        let szja_text = catalog.gettext("SZJA (személyi jövedelemadó)");
+        let munkaero_piaci_jarulek_text = catalog.gettext("Munkaerő-piaci járulék");
+        let netto_havi_ber_text = catalog.gettext("Nettó havi bér");
+        let result: String = format!("{}: \n\n{}: {:.2} Ft\n{}: {:.2} Ft\n{}: {:.2} Ft\n{}: {:.2} Ft\n{}: {:.2} Ft\n\n{}: {:.2} Ft", jarulekok_text, nyugdij_bizt_jarulek_text, calculated_jarulekok_map.get("nyugdij_bizt").unwrap(), penzbeni_egeszsegbizt_jarulek_text, calculated_jarulekok_map.get("penzbeni_egeszseg_bizt").unwrap(), termeszetbeni_egeszsegbizt_jarulek_text, calculated_jarulekok_map.get("term_egeszseg_bizt").unwrap(), szja_text, calculated_jarulekok_map.get("szja").unwrap(), munkaero_piaci_jarulek_text, calculated_jarulekok_map.get("munkaero_piaci").unwrap(), netto_havi_ber_text, netto_num);
         Ok(result)
     }
 
@@ -71,16 +84,24 @@ impl Berkalkulator {
 fn main() -> Result<(), slint::PlatformError> {
     slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/i18n/"));
 
+
     let ui: AppWindow = AppWindow::new()?;
     let ui_handle: Weak<AppWindow> = ui.as_weak();
     ui.on_divide_income(move |string: SharedString| {
         let ui: AppWindow = ui_handle.unwrap();
         let brutto_ber: f64 = string.trim().parse().unwrap();
-        let berkalkulator = Berkalkulator::calculate_netto_ber(brutto_ber);
-        match berkalkulator {
-            Ok(response) => ui.set_results(response.into()),
-            Err(e) => ui.set_results(e.into()),
-        }
+
+    let mo_file_path = concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/i18n/en/LC_MESSAGES/berkalkulator-rust.mo"
+    );
+    let file = File::open(mo_file_path).expect("could not open the catalog");
+    let catalog = Catalog::parse(file).expect("could not parse the catalog");
+    let berkalkulator = Berkalkulator::calculate_netto_ber(brutto_ber, catalog);
+    match berkalkulator {
+        Ok(response) => ui.set_results(response.into()),
+        Err(e) => ui.set_results(e.into()),
+    }
     });
     ui.run()
 }
