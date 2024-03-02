b2a601fad5b19b420a49706ba4c294529997f403
K3cs1 <kecskemetip@gmail.com>
2024-03-02 12:46:21 +0100

    Add csv export feature

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use log::{info, warn};
 use slint::{SharedString, Weak};
 use std::collections::HashMap;
 use std::fs::File;
+use std::error::Error;
+use csv::Writer;
 use sys_locale::get_locale;
+use serde::{Serialize, Deserialize};
 
 slint::include_modules!();
 
@@ -12,6 +15,7 @@ const ONE: f64 = 1.00;
 const ZERO: f64 = 0.00;
 const ONE_HUNDRED_MILLION: f64 = 100000000.00;
 const FIVE_THOUSAND: f64 = 5000.00;
+const CSV_PATH: &str = "export.csv";
 
 enum Jarulek {
     NyugdijBizt(f64),
@@ -21,13 +25,26 @@ enum Jarulek {
     MunkaeroPiaci(f64),
 }
 
-struct Berkalkulator {}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct CsvRecord {
+    net_monthly_salary: f64,
+    pension_insurance: f64,
+    cash_health_insurance: f64,
+    health_insurance: f64,
+    sjja: f64,
+    labor_market: f64,
+}
+
+struct Berkalkulator {    
+}
 
 impl Berkalkulator {
     fn calculate_netto_ber(
         brutto_ber: f64,
         friss_hazas: bool,
         szja_mentes: bool,
+        csv_export: bool,
         catalog: Catalog,
     ) -> Result<String, String> {
         if brutto_ber <= ZERO {
@@ -42,20 +59,20 @@ impl Berkalkulator {
         }
         let jarulekok: Vec<Jarulek> = Self::init_jarulekok();
         let mut sum_of_jarulekok: f64 = ZERO;
-        let mut calculated_jarulekok_map: HashMap<&str, f64> = HashMap::new();
+        let mut calculated_jarulekok_map: HashMap<String, f64> = HashMap::new();
         for jarulek in jarulekok.iter() {
             match jarulek {
                 Jarulek::NyugdijBizt(amount) => {
                     sum_of_jarulekok = sum_of_jarulekok + amount;
-                    calculated_jarulekok_map.insert("nyugdij_bizt", brutto_ber * amount);
+                    calculated_jarulekok_map.insert("nyugdij_bizt".to_owned(), brutto_ber * amount);
                 }
                 Jarulek::PenzbeniEgeszsegBizt(amount) => {
                     sum_of_jarulekok = sum_of_jarulekok + amount;
-                    calculated_jarulekok_map.insert("penzbeni_egeszseg_bizt", brutto_ber * amount);
+                    calculated_jarulekok_map.insert("penzbeni_egeszseg_bizt".to_owned(), brutto_ber * amount);
                 }
                 Jarulek::TermeszetbeniEgeszsegBizt(amount) => {
                     sum_of_jarulekok = sum_of_jarulekok + amount;
-                    calculated_jarulekok_map.insert("term_egeszseg_bizt", brutto_ber * amount);
+                    calculated_jarulekok_map.insert("term_egeszseg_bizt".to_owned(), brutto_ber * amount);
                 }
                 Jarulek::SZJA(amount) => {
                     let mut calculated_szja;
@@ -69,11 +86,11 @@ impl Berkalkulator {
                         calculated_szja = ZERO;
                         sum_of_jarulekok = sum_of_jarulekok - amount;
                     }
-                    calculated_jarulekok_map.insert("szja", calculated_szja);
+                    calculated_jarulekok_map.insert("szja".to_owned(), calculated_szja);
                 }
                 Jarulek::MunkaeroPiaci(amount) => {
                     sum_of_jarulekok = sum_of_jarulekok + amount;
-                    calculated_jarulekok_map.insert("munkaero_piaci", brutto_ber * amount);
+                    calculated_jarulekok_map.insert("munkaero_piaci".to_owned(), brutto_ber * amount);
                 }
             }
         }
@@ -105,6 +122,13 @@ impl Berkalkulator {
         netto_num);
 
         info!("{}", result);
+        if csv_export == true {
+            let res = Self::export_calculations_to_csv(&calculated_jarulekok_map, &netto_num);
+            match res {
+                Ok(()) => info!("export.csv created"),
+                Err(message) => warn!("{}", message)
+            }
+        }
         Ok(result)
     }
 
@@ -117,6 +141,22 @@ impl Berkalkulator {
             Jarulek::MunkaeroPiaci(0.015),
         ]
     }
+
+    fn export_calculations_to_csv(calculated_jarulekok_map: &HashMap<String, f64>, netto_num: &f64) -> Result<(), Box<dyn Error>> {
+        let mut writer = Writer::from_path(CSV_PATH)?;
+        writer.serialize(CsvRecord{
+            labor_market: *calculated_jarulekok_map.get("munkaero_piaci").unwrap(),
+            cash_health_insurance: *calculated_jarulekok_map.get("penzbeni_egeszseg_bizt").unwrap(),
+            health_insurance: *calculated_jarulekok_map.get("term_egeszseg_bizt").unwrap(),
+            sjja: *calculated_jarulekok_map.get("szja").unwrap(),
+            pension_insurance: *calculated_jarulekok_map.get("nyugdij_bizt").unwrap(),
+            net_monthly_salary: *netto_num
+        })?;
+
+        writer.flush()?;
+
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), slint::PlatformError> {
@@ -126,7 +166,7 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui: AppWindow = AppWindow::new()?;
     let ui_handle: Weak<AppWindow> = ui.as_weak();
     ui.on_divide_income(
-        move |string: SharedString, friss_hazas: bool, szja_mentes: bool| {
+        move |string: SharedString, friss_hazas: bool, szja_mentes: bool, csv_export: bool| {
             let ui: AppWindow = ui_handle.unwrap();
             let brutto_ber = string.trim().parse();
             let mut brutto_ber_num: f64 = ZERO;
@@ -154,6 +194,7 @@ fn main() -> Result<(), slint::PlatformError> {
                 brutto_ber_num,
                 friss_hazas,
                 szja_mentes,
+                csv_export,
                 catalog,
             );
             match berkalkulator {
